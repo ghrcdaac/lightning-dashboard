98dd007d8de2018d47755e23d667179838a7f7cb
alansubedi123 <alansubedi123@gmail.com>
2022-07-28 14:33:31 -0500

    HotSpot2.0 Toggle Changes

--- app/assets/scripts/components/global/panelComponents/HotSpotToggle.js
+++ app/assets/scripts/components/global/panelComponents/HotSpotToggle.js
@@ -4,22 +4,32 @@ import { useSelector, useDispatch } from "react-redux";
 import { truncated } from "../../../styles/helpers";
 import { FormSwitch } from "../../../styles/form/switch";
 import { changeHotspot } from "../../../redux/action/HotspotAction";
+import {FaSatellite} from "../../../../../../node_modules/react-icons/fa"
 
 const MainContainer = styled.div`
 display:flex;
-height:74px;
+height:60px;
 width:100%;
 border-bottom: 0.1px solid #E8E8E8;
 `
+const InnerContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height:100%;
+`
+
 const Swatch = styled.div`
-position:absolute;
-height:69px;
+//position:absolute;
+//height:69px;
+height:95%;
 margin-left:2px;
-margin-top:2px;
+//margin-top:2px;
 border-radius: 25px;
 width:3.8px;
 z-index:999999;
 background-color:#C0C0C0;
+//margin-bottom:5px;
 `
 const Main = styled.div`
 display:flex;
@@ -32,8 +42,8 @@ const LayerTitle = styled.h1`
   font-size: 1rem;
   line-height: 1.25rem;
   margin: 0;
-  margin-bottom:10px;
-  margin-left:20px;
+  //margin-bottom:10px;
+  margin-left:15px;
   sub {
     bottom: 0;
   }
@@ -50,23 +60,30 @@ const HotSpotToggle = ({activeLayers}) =>{
 
     return(
         <MainContainer>
+            <InnerContainer>
                 <Swatch />
-            <Main>
-                <div>
-                    <LayerTitle title={'HotSpots'}>HotSpots</LayerTitle>
-                </div>
-                <div style={{marginLeft:'140px'}}>
-                    <FormSwitch
-                    hideText
-                    //name={`toggle-${id}`}
-                    //disabled={disabled}
-                    checked={HOTSPOT}
-                    onChange={ToggleHandler}
-                    >
-                    Toggle layer visibility
-                    </FormSwitch>
-                </div>
-            </Main>
+                <Main>
+                    <div>
+                        <LayerTitle title={'HotSpots'}>
+                            <div style={{display:'flex'}}>
+                                <FaSatellite/> 
+                                <h3 style={{marginLeft:"15px"}}>HotSpots</h3>
+                            </div>
+                            </LayerTitle>
+                    </div>
+                    <div style={{marginLeft:'90px'}}>
+                        <FormSwitch
+                        hideText
+                        //name={`toggle-${id}`}
+                        //disabled={disabled}
+                        checked={HOTSPOT}
+                        onChange={ToggleHandler}
+                        >
+                        Toggle layer visibility
+                        </FormSwitch>
+                    </div>
+                </Main>
+            </InnerContainer>
         </MainContainer>
     )
 }
