98f76773de361d52f831e6971644f27a6fba4185
alansubedi123 <alansubedi123@gmail.com>
2022-09-29 10:52:33 -0500

    Changed file structure for datalayer

--- app/assets/scripts/components/common/AccordionLayer.js
+++ app/assets/scripts/components/common/AccordionLayer.js
@@ -4,8 +4,18 @@ import styled from 'styled-components';
 export const AccordionContainer = styled.div`
 display:flex;
 flex-direction:column;
-
 `
+export const DatasetSwatch = styled.div`
+position:absolute;
+height:49px;
+margin-left:2px;
+margin-top:1.3px;
+border-radius: 25px;
+width:3.8px;
+z-index:999999;
+background-color:#C0C0C0;
+`
+
 export const TitleWrapper = styled.div`
 display:flex;
 height:50px;
@@ -27,7 +37,17 @@ height:1px;
 background-color:#E8E8E8;
 `
 
-
 export const AccordionBody = styled.div`
-display:null;
+position:relative;
+//top:0px;
+//bottom:500px;
+`
+export const PanelTitle = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+text-align:center;
+font-weight: bold;
+height:50px;
+border:1px solid grey;
 `
--- app/assets/scripts/components/common/data-layers-block.js
+++ app/assets/scripts/components/common/data-layers-block.js
@@ -14,7 +14,8 @@ import {
 } from './panel-block';
 import Layer from './layer';
 import { Accordion } from './accordion';
-import {AccordionContainer, AccordionTitle, AccordionBody, TitleWrapper, BottomLineAccordion} from './AccordionLayer';
+import {AccordionContainer, AccordionTitle, AccordionBody, TitleWrapper, BottomLineAccordion, DatasetSwatch, PanelTitle} from './AccordionLayer';
+import { TitleBlock, TitleName, LayerTitle, LinkTitle, BottomLine, LinkContainer, Link, LayerBody, LinkBody, MainBody } from './LayerLink';
 
 import Slider from '../MiniComponents/Single/Slider';
 import BaselineToggle from '../MiniComponents/BaselineLayer/BaselineToggle';
@@ -33,148 +34,13 @@ const PanelBlockLayer = styled(PanelBlock)`
 
 const Swatch = styled.div`
 position:absolute;
-height:47px;
+height:49px;
 margin-left:2px;
 margin-top:1.3px;
 border-radius: 25px;
 width:3.8px;
 z-index:999999;
 background-color:#C0C0C0;
-`
-
-const TitleBlock = styled.div`
-display:flex;
-justify-content:space-around;
-//background-color:red;
-width:100%;
-transition:slideRight 0.6s ease;
-`
-const TitleName = styled.div`
-text-align:center;
-:hover{
-  cursor:pointer;
-  color:${COLOR};
-}
-//left:0;
-color:${(props)=>props.color}
-`
-const LayerTitle = styled.div`
-display:flex;
-flex-direction:column;
-width:50%;
-margin-right:10px;
-transition:1s;
-left:0;
-justify-content:center;
-align-items:center;
-color:${(props)=>props.color};
-transition:slideRight 0.6s ease;
--webkit-animation: fadein 3s;
--moz-animation: fadein 3s; 
--ms-animation: fadein 3s;
--o-animation: fadein 3s; 
-animation: fadein 3s; 
-
-:hover{
-  color:#2276AC;
-}
-`
-
-const LinkTitle = styled.div`
-display:flex;
-flex-direction:column;
-width:50%;
-margin-left:10px;
-transition:1s;
-left:0;
-justify-content:center;
-align-items:center;
-:hover{
-  color:#2276AC;
-}
-color:${(props)=>props.color}
-`
-const BottomLine = styled.div`
-width:124%;
-height:3px;
-background-color:grey;
-//transition:1s;
-left:${(props)=>props.left};
-justify-content:center;
-align-items:center;
-background-color:#2276AC;
-position:relative;
-top:10px;
--webkit-animation: ${(props)=>props.animation};
--moz-animation: ${(props)=>props.animation}; 
--ms-animation: ${(props)=>props.animation};
--o-animation: ${(props)=>props.animation}; 
-animation: ${(props)=>props.animation}; 
-
-@keyframes fadein-title {
-  0% { left:0px }
-  25% { left:34.5px }
-  50% {left:69px}
-  75% {left:103.5px}
-  100% {left:138px}
-}
-
-@keyframes fadeout-title {
-  0% { left:138px }
-  25% {left:103.5px}
-  50% {left:69px}
-  75% { left:34.5px }
-  100% { left:0px }
-}
-`
-const LinkContainer = styled.div`
-//background-color:red;
-padding-top:5px;
-width:17rem;
-`
-
-const Link = styled.div`
-padding-bottom:15px;
-padding-left:15px;
-padding-right:15px;
-//background-color:white;
-font-weight:bold;
-width:17rem;
-`
-
-const LayerBody = styled.div`
-width:70%;
-`
-const LinkBody = styled.div`
-width:30%;
-`
-const MainBody = styled.div`
-display:flex;
-position:relative;
-width:25.8rem;
-//left:-280px;
-left:${(props)=>props.left};
--webkit-animation: ${(props)=>props.animation};
--moz-animation: ${(props)=>props.animation}; 
--ms-animation: ${(props)=>props.animation};
--o-animation: ${(props)=>props.animation}; 
-animation: ${(props)=>props.animation}; 
-
-@keyframes fadein-body {
-  0% { left:0px }
-  25% { left:-70px }
-  50% {left:-140px}
-  75% {left:-210px}
-  100% {left:-280px}
-}
-
-@keyframes fadeout-body {
-  0% {left:-280px}
-  25% {left:-210px}
-  50% {left:-140px}
-  75% { left:-70px }
-  100% { left:0px }
-}
 `
 
 class DataLayersBlock extends React.Component {
@@ -192,7 +58,7 @@ class DataLayersBlock extends React.Component {
       leftBody:'0px',
 
       //for layerDropDown
-      dropdown: 'null'
+      dropdown: 'TRMM LIS'
     };  
     
     this.layerHandler = this.layerHandler.bind(this);
@@ -256,11 +122,12 @@ class DataLayersBlock extends React.Component {
         </PanelBlockHeader>
         <PanelBlockBody>
           <PanelBlockScroll>
+            {/* <PanelTitle>Datasets</PanelTitle> */}
             <MainBody left={this.state.leftBody} animation={this.state.animationBody}>
               <LayerBody>
                 {two_dim_layers.map((layerArray)=>(
-                  <AccordionContainer>
-                    <Swatch/>
+                  <AccordionContainer key={layerArray[0].dataset_type}>
+                    <DatasetSwatch/>
                     <TitleWrapper onClick={()=>this.dropdownHandler(layerArray[0].dataset_type)}>
                       <AccordionTitle>{layerArray[0].dataset_type}</AccordionTitle>
                       {this.state.dropdown === layerArray[0].dataset_type && <RiArrowDropUpLine size={40}/>}
@@ -304,6 +171,7 @@ class DataLayersBlock extends React.Component {
                     }
                   </AccordionContainer>
                 ))}
+                {/* <PanelTitle>Features</PanelTitle> */}
                 <Slider slideHandler={tileOpacity}/>
                 <BaselineToggle 
                 calendarStatus={calendarStatus} 
