2734637fabf2225c73a2f0963d7aa1142583471c
alansubedi123 <alansubedi123@gmail.com>
2023-08-18 11:38:29 -0500

    layerPanel slider with animation added

--- app/assets/scripts/components/MiniComponents/Layer/LayerPanel.js
+++ app/assets/scripts/components/MiniComponents/Layer/LayerPanel.js
@@ -1,77 +1,188 @@
-import React,{useState} from 'react';
-import styled, { withTheme, ThemeProvider } from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import LayerCard from './LayerCard';
-// import { layer } from '../../../data/layers.json';
+import {FiArrowRight,FiArrowLeft} from "../../../../../../node_modules/react-icons/fi"
+import {MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft} from "../../../../../../node_modules/react-icons/md"
 
-const Container = styled.div`
-width:90%;
+const OuterContainer = styled.div`
+width:100%;
 height:120px;
 background-color: none;
 position: absolute;
 display:flex;
 flex-direction:row;
-overflow-x:auto;
 bottom:${(props)=>props.bottom};
-margin-left:100px;
-
-overflow-y: auto;
-scrollbar-width: thin; /* For Firefox */
-scrollbar-color: #ccc transparent; /* For Firefox */
-
-&::-webkit-scrollbar {
-  width: 8px; /* Adjust as needed */
-  height:8px;
-}
-
-&::-webkit-scrollbar-track {
-  background: transparent;
-}
-
-&::-webkit-scrollbar-thumb {
-  background-color: #ccc;
-  border-radius: 4px;
-}
-
-&::-webkit-scrollbar-thumb:hover {
-  background-color: #888;
-}
-
-&:hover {
-  &::-webkit-scrollbar-thumb {
-    background-color: #888;
-  }
-}
-
-&:not(:hover) {
-  &::-webkit-scrollbar-thumb {
-    background-color: #ccc;
-  }
-}
+// background-color:red;
+justify-content:center;
 `
 
-//TODO: add arrow sliders on left and right
-const LayerPanel = ({ onAction, layers, activeLayer }) =>{
+const LeftScrollBtn = styled.button`
+  height: 100%;
+  width: 3%;
+  margin-right: 15px;
+  background-color: 	rgb(168,168,168,0.2);
+  border: none;
+  transition: background-color 0.3s;
 
-    var bottom = '20px'
-    if(activeLayer.length !== 0) {
-        bottom = '150px'
+  &:hover {
+    background-color: 	rgb(48,48,48, 1); /* Darkish gray */
+    cursor:pointer;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 90%;
+  height: 100%;
+  // background-color: red;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  overflow-x: hidden;
+  bottom: ${(props) => props.bottom};
+  overflow-y: hidden; /* Hide vertical scrollbar */
+  scrollbar-width: none; /* Hide horizontal scrollbar for Firefox */
+  -ms-overflow-style: none; /* Hide horizontal scrollbar for IE and Edge */
+  // background-color:blue;
+  &::-webkit-scrollbar {
+    width: 8px; /* Adjust as needed */
+    height:8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #ccc;
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #888;
+  }
+
+  &:hover {
+    &::-webkit-scrollbar-thumb {
+      background-color: #888;
     }
+  }
 
-    const clickHandler = (layer) => {
-        onAction('layer.toggle', layer)
+  &:not(:hover) {
+    &::-webkit-scrollbar-thumb {
+      background-color: #ccc;
     }
+  }
+`;
 
-    return (
-        <Container bottom={bottom}>
-            {layers.map(( layer )=>(
-                <LayerCard 
-                clicked={clickHandler}
-                layer={layer}
-                activeLayer={activeLayer}
-                />
-            ))}
-        </Container>
-    )
-}
+const RightScrollBtn = styled.button`
+  height: 100%;
+  width: 3%;
+  margin-left: 13px;
+  background-color: 	rgb(168,168,168,0.2);
+  border: none;
+  transition: background-color 0.3s;
 
-export default LayerPanel
+  &:hover {
+    background-color: 	rgb(48,48,48, 1); /* Darkish gray */
+    cursor:pointer;
+  }
+`;
+
+// Define the keyframes for the animation
+const slideAnimation = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100px); /* Adjust the distance as needed */
+  }
+`;
+
+const SliderButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 30px;
+  height: 30px;
+  background-color: #ccc;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  z-index: 1;
+`;
+
+const LeftSliderButton = styled(SliderButton)`
+  left: 5px;
+`;
+
+const RightSliderButton = styled(SliderButton)`
+  right: 5px;
+`;
+
+const LayerPanel = ({ onAction, layers, activeLayer }) => {
+  var bottom = '20px';
+  if (activeLayer.length !== 0) {
+    bottom = '150px';
+  }
+
+  const clickHandler = (layer) => {
+    onAction('layer.toggle', layer);
+  };
+
+  const scrollContainerLeft = () => {
+    const container = document.getElementById('layer-container');
+    if (container) {
+      let scrollAmount = container.scrollLeft;
+      const increment = -800; // Negative value to scroll left
+  
+      const targetScrollAmount = scrollAmount + increment;
+      const scrollInterval = setInterval(() => {
+  
+        if (scrollAmount > targetScrollAmount) {
+          scrollAmount += -13; // Decrement by -1 or a smaller value for smoother scrolling
+          container.scrollLeft = scrollAmount;
+        } else {
+          clearInterval(scrollInterval); // Stop the interval once target is reached
+        }
+      }, 1);
+    }
+  };
+  
+
+  const scrollContainerRight = () => {
+    const container = document.getElementById('layer-container');
+    if (container) {
+      let scrollAmount = container.scrollLeft;
+      const increment = 800; // Adjust the increment as needed
+  
+      const targetScrollAmount = scrollAmount + increment;
+
+      const scrollInterval = setInterval(() => {
+  
+        if (scrollAmount < targetScrollAmount) {
+          scrollAmount += 13; // Increment by 1 or a smaller value for smoother scrolling
+          container.scrollLeft = scrollAmount;
+        } else {
+          clearInterval(scrollInterval); // Stop the interval once target is reached
+        }
+      }, 1);
+    }
+  };
+  
+
+  return (
+    <OuterContainer bottom={bottom}>
+      <LeftScrollBtn onClick={scrollContainerLeft}><MdOutlineKeyboardArrowLeft size={25}/></LeftScrollBtn>
+      <Container id="layer-container">
+        {layers.map((layer) => (
+          <LayerCard clicked={clickHandler} layer={layer} activeLayer={activeLayer} />
+        ))}
+      </Container>
+      <RightScrollBtn onClick={scrollContainerRight}><MdOutlineKeyboardArrowRight size={25}/> </RightScrollBtn>
+    </OuterContainer>
+  );
+};
+
+export default LayerPanel;
