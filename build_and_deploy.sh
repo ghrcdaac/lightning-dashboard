c5365cb05ecb4a0ff5f1fb489439ffb9084da849
Navaneeth R Selvaraj <ns0066@uah.edu>
2023-10-04 11:10:09 -0500

    Update build_and_deploy.sh

---
+++ build_and_deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e  # Abort script if any command fails
+
+export AWS_REGION="$bamboo_AWS_REGION"
+export AWS_ACCESS_KEY_ID="$bamboo_AWS_SVC_PROD_ACCESS_KEY"
+export AWS_SECRET_ACCESS_KEY="$bamboo_AWS_SVC_PROD_SECRET_ACCESS_KEY"
+
+# Check if AWS credentials are valid
+aws sts get-caller-identity >/dev/null
+
+# Build the Docker image
+docker build . -t lightdash
+
+# Create a unique container ID
+CID=$(docker create lightdash)
+
+# Copy the build artifacts from the Docker container to the local "dist" directory
+docker cp "${CID}":app/build ./dist
+
+# Clean up the Docker container
+docker rm "${CID}"
+
+# Validate AWS S3 connectivity
+aws s3 ls >/dev/null
+
+# Move the contents of the "lightdash" directory in the S3 bucket to a backup directory
+# aws s3 mv s3://ghrc-web-services/lightdash s3://ghrc-web-services-backup/lightdash --recursive
+
+# # Sync the local "dist" directory to the "lightdash" directory in the S3 bucket
+# aws s3 sync ./dist s3://ghrc-web-services/lightdash
+
+# Cleanup the "dist" directory
+rm -rf ./dist
+
+echo "Deployment completed successfully."
